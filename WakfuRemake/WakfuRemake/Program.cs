7d6153d17a2187cefada08fa7e8d7da22803b3aa
Raphael <raphael.goulmot@epitech.eu>
2017-10-14 00:57:31 +0200

    Adding all folder and the AuthServer class

---
+++ WakfuRemake/WakfuRemake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WakfuRemake
+{
+    class Program
+    {
+        public static Thread AuthThread { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main: Initialisation...");
+            Console.Read();
+        }
+    }
+}
