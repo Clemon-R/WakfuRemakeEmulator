6eb8015af4feb7c1ff0bb202795375ca882fce0d
Raphael <raphael.goulmot@epitech.eu>
2017-10-15 13:26:49 +0200

    Up

---
+++ WakfuRemake/WakfuRemake/Auth/Messages/Sender/Version.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WakfuRemake.Common.BigEndian;
+
+namespace WakfuRemake.Auth.Messages.Sender
+{
+    public class Version
+    {
+        public static BigEndianWriter SendVersion(AuthClient client, byte version, ushort revision, byte change, string build)
+        {
+            BigEndianWriter packet = new BigEndianWriter();
+            packet.WriteByte(0);
+            packet.WriteUShort(7);
+            packet.WriteByte(version);
+            packet.WriteUShort(revision);
+            packet.WriteByte(change);
+            packet.WriteString(build);
+            packet.Seek(0, System.IO.SeekOrigin.Begin);
+            packet.WriteUShort((ushort)packet.Data.Length);
+            return (packet);
+        }
+    }
+}
